0464b6785ac9aa919dbaf4315e4e6b0dc8b8fd4e
EllianRodrigues <104794781+EllianRodrigues@users.noreply.github.com>
2023-04-25 20:53:59 -0300

    Pasta IMG com sprites

--- version_1.py
+++ version_1.py
@@ -9,10 +9,16 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption('unnamed')
 
+x = 200
+y = 200
+img = pygame.image.load('img/player/Idle/0.png')
+rect = img.get_rect()
+rect.center = (x, y)
+
 
 run = True
 while run:
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
