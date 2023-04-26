dd2fdb389f8dfa639b7448992029e7e2b678701a
yuumi kunii <yuumikunii1403@gmail.com>
2023-04-26 12:52:37 -0300

    Animações e pulo

--- version_1.py
+++ version_1.py
@@ -11,27 +11,46 @@ pygame.display.set_caption('unnamed')
 
 clock = pygame.time.Clock()
 FPS = 60
+GRAVITY = 0.75
 
 moving_left = False
 moving_right = False
 
 BG = (144, 201, 120)
-
+RED = (255, 0, 0)
 
 def draw_bg():
     screen.fill(BG)
+    pygame.draw.line(screen, RED, (0, 300), (screen_width, 300))
 
 
 class Soldier(pygame.sprite.Sprite):
     def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
         self.char_type = char_type
         self.speed = speed
         self.direction = 1
+        self.vel_y = 0
+        self.jump = False
         self.flip = False
-        img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
-        self.image = pygame.transform.scale(
-            img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+        temp_list = []
+        for i in range(5):
+            img = pygame.image.load(f'img/{self.char_type}/Idle/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        temp_list = []
+        for i in range(6):
+            img = pygame.image.load(f'img/{self.char_type}/Run/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            temp_list.append(img)
+        self.animation_list.append(temp_list)
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -50,9 +69,49 @@ class Soldier(pygame.sprite.Sprite):
             self.flip = False
             self.direction = 1
 
+        #jump
+        if self.jump == True:
+            self.vel_y = -11
+            self.jump = False
+
+
+        #apply gravity
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+
+
+        #check collision with floor
+        if self.rect.bottom + dy > 300:
+            dy = 300 - self.rect.bottom
+
         # update rectangle position
         self.rect.x += dx
         self.rect.y += dy
+    
+
+    def update_animation(self):
+        # update animation
+        ANIMATION_COOLDOWN = 100
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        #loop the animation
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+
+    def update_action(self, new_action):
+        #check if the new action is different
+        if new_action != self.action:
+            self.action = new_action
+            #update animation settings 
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+
 
     def draw(self):
         screen.blit(pygame.transform.flip(
@@ -70,10 +129,17 @@ while run:
 
     draw_bg()
 
+    player.update_animation()
     player.draw()
     enemy.draw()
 
-    player.move(moving_left, moving_right)
+    #update player's action
+    if player.alive:
+        if moving_left or moving_right:
+            player.update_action(1) #run
+        else:
+            player.update_action(0) #idled
+        player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
         # quit game
@@ -85,6 +151,8 @@ while run:
                 moving_left = True
             if event.key == pygame.K_d:
                 moving_right = True
+            if event.key == pygame.K_w and player.alive:
+                player.jump = True
             if event.key == pygame.K_ESCAPE:
                 run = False
 
