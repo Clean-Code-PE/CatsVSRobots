873ad3d9480f079d919a553765d11fda4f26f0f9
miguel-oliveiraa <miguellgomes64@gmail.com>
2023-05-01 09:20:29 -0300

    import background imgs

--- version_1.py
+++ version_1.py
@@ -28,6 +28,12 @@ shoot = False
 grenade = False
 grenade_tick = False
 
+# load images
+pine1_img = pygame.image.load('img/background/pine1.png').convert_alpha()
+pine2_img = pygame.image.load('img/background/pine2.png').convert_alpha()
+pine1_img = pygame.image.load('img/background/mountain.png').convert_alpha()
+sky_img = pygame.image.load('img/background/sky_cloud.png').convert_alpha()
+
 
 #store tiles in a list
 img_list = []
